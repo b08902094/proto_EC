8c9fe98d93993e31acc54ad6aeae4e5ba686c1d2
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-10-26 06:14:12 +0000

    add/Boot header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_homes_top_path
+    when Customer
+      root_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin
+      new_admin_session_path
+    when :customer
+      root_path
+    else
+      root_path
+    end
+  end
+
 end
