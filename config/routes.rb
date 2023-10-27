87a48705f69a7eef1a5233aafb639807a896a46b
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-10-27 08:08:04 +0000

    add/顧客のマイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 顧客用
@@ -7,20 +16,27 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
+  namespace :admin do
+    get 'homes/top'
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+
   scope module: :public do
     root to: 'homes#top'
     get 'homes/about'
-  end
-  
-  namespace :admin do
-    get 'homes/top'
+    resources :customers, only: [:show, :edit, :update] do
+      member do
+        get 'confirm'
+        patch 'withdraw'
+      end
+    end
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails dbseed command (or created alongside the database with dbsetup).
 #
-# Examples:
+# Examples
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name 'Star Wars' }, { name 'Lord of the Rings' }])
+#   Character.create(name 'Luke', movie movies.first)
 
 Admin.create!(email: 'admin@example.com', password: 'password')
+
+Customer.create!(
+  last_name: '令和',
+  first_name: '道子',
+  last_name_kana: 'レイワ',
+  first_name_kana: 'ミチコ',
+  post_code: '000000',
+  address: '東京都渋谷区神園町0-0',
+  phone_number: '00000000',
+  is_active: true
+)
